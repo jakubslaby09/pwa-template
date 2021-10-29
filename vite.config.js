dd5dd20edc4f177bd60236ef6b0f73757956e224
jakubslaby09 <jakubslaby09@gmail.com>
2021-10-29 18:58:15 +0200

    fix building views

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
-/**
- * @type {import('vite').UserConfig}
- */
-const config = {
+import { readdirSync } from 'fs'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   server: {
       host: true,
   },
   build: {
-      target: 'es2021',
+    target: 'es2021',
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: [
+        resolve(__dirname, 'index.html'),
+        ...readdirSync(
+          resolve(__dirname, 'views')
+        ).map(file => resolve(__dirname, 'views', file)),
+      ]
+    }
   },
-}
-
-export default config
+})
