2b9e4249b83c8e0afac254fa50ed9451f8fcdb8e
jakubslaby09 <jakubslaby09@gmail.com>
2021-11-11 14:10:00 +0100

    replace `.wav` files with `.flac`s

--- src/modules/sound.ts
+++ src/modules/sound.ts
@@ -1,15 +1,15 @@
 export const sounds = {
     tap: new Audio('sounds/tap1.wav'),
-    navForward: new Audio('sounds/nav1.wav'),
-    refresh: new Audio('sounds/refresh.wav'),
-    shutter: new Audio('sounds/shutter.wav'),
+    navForward: new Audio('sounds/nav1.flac'),
+    refresh: new Audio('sounds/refresh.flac'),
+    shutter: new Audio('sounds/shutter.flac'),
 }
 sounds.tap.volume = 0.5
 sounds.navForward.volume = 0.1
 sounds.shutter.volume = 0.05
 
-export async function tap() {
-    await sounds.tap.play()
+export function tap() {
+    sounds.tap.play()
     //navigator.vibrate([0, 150, 5])
 }
 
