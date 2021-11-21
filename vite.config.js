4879900c897f73886abca90a03336e1a77ad44a6
jakubslaby09 <jakubslaby09@gmail.com>
2021-11-21 09:11:35 +0100

    remove unused plugin to fix CVE-2021-23807

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { readdirSync } from 'fs'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   server: {
