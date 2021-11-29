3a84ef5e40afa8ad14b0bf465715baff5829ca36
jakubslaby09 <jakubslaby09@gmail.com>
2021-11-29 20:11:16 +0100

    fix sw error while using http

--- src/modules/offline.ts
+++ src/modules/offline.ts
@@ -1,18 +1,20 @@
-/* const worker =  */location.port != '3000'
-    ? navigator.serviceWorker?.register(new URL('/sw.js', import.meta.url).href)
+if(navigator.serviceWorker) {
+    /* const worker =  */location.port != '3000'
+    ? navigator.serviceWorker.register(new URL('/sw.js', import.meta.url).href)
     : null
-
-navigator.serviceWorker.onmessage = e => {
-    if(e.data == 'update') {
-        location.reload()
+    
+    navigator.serviceWorker.onmessage = e => {
+        if(e.data == 'update') {
+            location.reload()
+        }
     }
-}
-
-const update = async () => {
-    if(!navigator.onLine) {
-        console.warn('%cCannot update caches while offline!', 'color: orange');
-        return
+    
+    const update = async () => {
+        if(!navigator.onLine) {
+            console.warn('%cCannot update caches while offline!', 'color: orange');
+            return
+        }
+        navigator.serviceWorker.controller?.postMessage('update')
     }
-    navigator.serviceWorker.controller?.postMessage('update')
+    (window as any).updateCaches = update
 }
-(window as any).updateCaches = update
