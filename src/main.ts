52ea9405905da1422a9dff2370951d8e2de62da7
jakubslaby09 <jakubslaby09@gmail.com>
2022-06-23 19:26:46 +0200

    remove duplicate import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './style.css'
 import './modules/elements'
 import './modules/router'
 import './modules/offline'
-import './modules/router'
 import * as sounds from './modules/sound'
 
 (window as any).sounds = sounds
