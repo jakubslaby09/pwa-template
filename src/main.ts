ba356531b1611de90da4e0e7a34508c512f2d623
Jakub Slabý <jakubslaby09@gmail.com>
2024-01-17 13:21:10 +0100

    use alpinejs from npm

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+import Alpine from 'alpinejs'
 import './style.css'
 import './modules/elements'
 import './modules/router'
 import './modules/offline'
 import * as sounds from './modules/sound'
 
-(window as any).sounds = sounds
+(window as any).sounds = sounds;
+(window as any).Alpine = Alpine
+
+Alpine.start()
